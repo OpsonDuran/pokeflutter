a9ebc1e261508bbac3bc9fe08e592bfdc5f80d48
armando.duran2 <armando.duran2@bbva.com>
2019-03-12 01:43:48 -0600

    basic pokedex with pokeAPI

---
+++ lib/detail.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:pokeflutter/pokemon.dart';
+
+class Detail extends StatelessWidget{
+  final Pokemon pokemon;
+
+  Detail({this.pokemon});
+
+  bodyWidget(BuildContext context)=>Stack(
+    children: <Widget>[
+      Positioned(
+        height: MediaQuery.of(context).size.height/1.5,
+        width: MediaQuery.of(context).size.width-20,
+        left: 10.0,
+        top: MediaQuery.of(context).size.height*0.1,
+        child: Card(
+          shape:RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(15.0),
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: <Widget>[
+                SizedBox(
+                  height: 70.0,
+                ),
+                Text(pokemon.name,style: TextStyle(fontSize: 15.0,fontWeight: FontWeight.bold),),
+                Text("Altura: ${pokemon.height}"),
+                Text("Peso: ${pokemon.weight}"),
+                Text("Tipo",style: TextStyle(fontWeight: FontWeight.bold)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: pokemon.type.map((t) => FilterChip(
+                      backgroundColor: Colors.amber,
+                      label: Text(
+                        t,
+                        style: TextStyle(color: Colors.white),),
+                      onSelected: (b) {}))
+                      .toList(),
+                ),
+                Text("Debilidad",style: TextStyle(fontWeight: FontWeight.bold)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: pokemon.weaknesses.map((t) => FilterChip(
+                      backgroundColor: Colors.red,
+                      label: Text(
+                        t,
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      onSelected: (b) {}))
+                      .toList(),
+                ),
+                Text("Evoluciones",style: TextStyle(fontWeight: FontWeight.bold)),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: pokemon.nextEvolution?.map((n) => FilterChip(
+                      backgroundColor: Colors.lightBlueAccent,
+                      label: Text(
+                          n.name,
+                          style: TextStyle(color: Colors.white),
+                      ),
+                      onSelected: (b) {}))?.toList()??[Text(pokemon.name)],
+                )
+              ],
+            ),
+          ),
+      ),
+
+
+      Align(
+        alignment: Alignment.topCenter,
+        child: Hero(
+            tag: pokemon.img,
+            child: Container(
+              height: 180.0,
+              width: 180.0,
+              decoration: BoxDecoration(
+                  image: DecorationImage(
+                      fit: BoxFit.cover, image: NetworkImage(pokemon.img))),
+            )),
+      )
+    ],
+  );
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      backgroundColor: Colors.redAccent,
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: Colors.redAccent,
+        title: Text(pokemon.name),
+      ),
+      body: bodyWidget(context),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,93 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:pokeflutter/pokemon.dart';
+import 'package:pokeflutter/detail.dart';
+import 'dart:convert';
+
+void main() => runApp(MaterialApp(
+  title: "Poke Flutter",
+  home: HomePage(),
+  debugShowCheckedModeBanner: false,
+));
+
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState(){
+    return new HomePageState();
+  }
+}
+
+class HomePageState extends State<HomePage> {
+  var url = "https://raw.githubusercontent.com/Biuni/PokemonGO-Pokedex/master/pokedex.json";
+  PokeHub pokeHub;
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  fetchData() async{
+    var res = await http.get(url);
+    var decodedJson = jsonDecode(res.body);
+    pokeHub = PokeHub.fromJson(decodedJson);
+    setState(() {});
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+          title: Text("Pokedex with Fultter"),
+          backgroundColor: Colors.redAccent
+      ),
+      body: pokeHub == null?Center(child: CircularProgressIndicator(),):
+      GridView.count(
+        crossAxisCount: 2,
+        children: pokeHub.pokemon.map((poke)=> Padding(
+          padding: const EdgeInsets.all(2.0),
+          child: InkWell(
+            onTap: (){
+              Navigator.push(context,
+                  MaterialPageRoute(
+                      builder: (context)=> Detail(
+                        pokemon: poke,
+                      )));
+            },
+            child: Hero(
+              tag: poke.img,
+              child: Card(
+                elevation: 3.0,
+                child: Column(
+                  mainAxisAlignment:MainAxisAlignment.spaceEvenly ,
+                  children: <Widget>[
+                    Text(
+                      poke.num,
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold),
+                    ),
+                    Container(
+                      height: 100.0,
+                      width: 100.0,
+                      decoration: BoxDecoration(
+                        image: DecorationImage(
+                            image:NetworkImage(poke.img) )
+                      ),
+                    ),
+                    Text(
+                      poke.name,
+                      style: TextStyle(
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.bold,
+
+                      ) )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        )).toList(),
+      ),
+    );
+  }
+}
